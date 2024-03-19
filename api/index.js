251133ac198f335817d61028b0a5520de4fedb89
Parth Patel <202001147@daiict.ac.in>
2024-03-19 18:44:15 +0530

    added sign up page UI

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authenRoutes from './routes/authen.route.js';
 
 dotenv.config();
-
+//connectiong to database
 mongoose
     .connect(process.env.MONGO, {serverSelectionTimeoutMS: 10000})
     .then(() => {
@@ -15,14 +15,25 @@ mongoose
         console.log('Error:', error);
     });
 
-
+//creating express app
 const app = express();
 
 app.use(express.json());
-
+//starting server on port 3000
 app.listen(3000, () => {
     console.log('Server is running on port 3000...');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/authen', authenRoutes);
+
+//adding middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    });
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Label, TextInput } from 'flowbite-react'
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left side */}
+        <div className='flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-teal-500 rounded text-gray-200'>Tech's</span>Blog
+          </Link>
+
+          <p className='mt-5 text-sm'>
+            This is the Tech Blog website devloped using MERN stack. You can Sign up using email and password or with google account.
+          </p>
+        </div>
+
+        {/* right side */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username'/>
+              <TextInput type='text' placeholder='Username'
+              id='username' />
+            </div>
+            <div>
+              <Label value='Your email'/>
+              <TextInput type='text' placeholder='name@company.com'
+              id='email' />
+            </div>
+            <div>
+              <Label value='Your password'/>
+              <TextInput type='text' placeholder='Password'
+              id='password' />
+            </div>
+            <Button gradientDuoTone='greenToBlue' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account already?</span>
+            <Link to='/sign-in' className='text-blue-500'> Sign In</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
