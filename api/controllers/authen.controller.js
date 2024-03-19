547baab9bdd75b76946cf53b946f021a60d29796
Parth Patel <202001147@daiict.ac.in>
2024-03-19 17:55:00 +0530

    add a middleware and error handler function

--- api/controllers/authen.controller.js
+++ api/controllers/authen.controller.js
@@ -1,23 +1,22 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signingup = async (req, res) => {
+export const signingup = async (req, res, next) => {
     //taking input from user and store inside variables
     const {username, email, password} = req.body;
     //handling null error
     if(!username || !email || !password || username==='' || email==='' || password==='') {
-        return res.status(400).json({message : "All fields are required"});
+        next(errorHandler(400, 'All fields are required'));
     }
     //hashing password with 10 salting round
     const hashPassword = bcryptjs.hashSync(password, 10);
     //creating new user from schema
     const newUser = new User({username, email, password: hashPassword});
 
-    try { // saving in database
-        await newUser.save();
+    try { 
+        await newUser.save(); // saving to database
+        res.json('Sign up Successful'); //success message
     } catch (error) { //handling error
-        return res.status(409).json({message : error.message});
+        next(error);
     }
-    //succesful message
-    res.json('Sign up Successful');
 };
