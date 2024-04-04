0fa4d2ec9dea4fc55b4d038df59ca9464782a0fa
Parth Patel <202001147@daiict.ac.in>
2024-04-04 19:39:09 +0530

    complete create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -16,10 +16,7 @@ export const create = async (req, res, next) => {
 
     try {
         const savedPost = await newPost.save();
-        res.status(201).json({
-            success: true,
-            post: savedPost,
-        });
+        res.status(201).json(savedPost);
     } catch (error) {
         next(error);
     }
