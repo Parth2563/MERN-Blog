9d84e5daac0d36057072ebe803c7cd4e4402083c
Parth Patel <202001147@daiict.ac.in>
2024-05-01 20:16:06 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authenRoutes from './routes/authen.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 //connectiong to database
@@ -17,6 +18,8 @@ mongoose
     .catch((error) => {
         console.log('Error:', error);
     });
+
+    const __dirname = path.resolve();
 
 //creating express app
 const app = express();
@@ -32,6 +35,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/authen', authenRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 //adding middleware
 app.use((err, req, res, next) => {
